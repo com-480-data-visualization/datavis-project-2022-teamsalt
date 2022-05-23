38138fd16c77f8bb43ffcb22f82d73be179297d0
Omid Karimi <omid.karimi@epfl.ch>
2022-05-23 21:21:02 +0200

    added flickering effect to projector

--- main.js
+++ main.js
@@ -1,42 +1,109 @@
-// Load dataset
-//var movie_path = "https://github.com/com-480-data-visualization/datavis-project-2022-teamsalt/tree/main/data/movie_dataset.csv"
-//var actor_path = "https://github.com/com-480-data-visualization/datavis-project-2022-teamsalt/tree/main/data/actor_dataset.csv"
+// Copyright 2021 Observable, Inc.
+// Released under the ISC license.
+// https://observablehq.com/@d3/normalized-stacked-area-chart
+function StackedAreaChart(data, {
+  x = ([x]) => x, // given d in data, returns the (ordinal) x-value
+  y = ([, y]) => y, // given d in data, returns the (quantitative) y-value
+  z = () => 1, // given d in data, returns the (categorical) z-value
+  marginTop = 20, // top margin, in pixels
+  marginRight = 30, // right margin, in pixels
+  marginBottom = 30, // bottom margin, in pixels
+  marginLeft = 40, // left margin, in pixels
+  width = 640, // outer width, in pixels
+  height = 400, // outer height, in pixels
+  xType = d3.scaleUtc, // type of x-scale
+  xDomain, // [xmin, xmax]
+  xRange = [marginLeft, width - marginRight], // [left, right]
+  yType = d3.scaleLinear, // type of y-scale
+  yDomain, // [ymin, ymax]
+  yRange = [height - marginBottom, marginTop], // [bottom, top]
+  zDomain, // array of z-values
+  offset = d3.stackOffsetExpand, // stack offset method
+  order = d3.stackOrderNone, // stack order method
+  yLabel, // a label for the y-axis
+  xFormat, // a format specifier string for the x-axis
+  yFormat = "%", // a format specifier string for the y-axis
+  colors = d3.schemeTableau10, // an array of colors for the (z) categories
+} = {}) {
+  // Compute values.
+  const X = d3.map(data, x);
+  const Y = d3.map(data, y);
+  const Z = d3.map(data, z);
 
-// const movie_promise = d3.csv("data/movie_dataset.csv").then((data) => {
-//   return {
-//     tid: data.tconst,
-//     averageRating: data.averageRating,
-//     numVotes: data.numVotes,
-//     title: data.primaryTitle,
-//     year: data.startYear,
-//     runtime: data.runtimeMinutes,
-//     genre: data.genres
-//     };
-// });
-//
-// const actor_promise = d3.csv("data/actor_dataset.csv").then((data) => {
-//   return {
-//     nid: data.nconst,
-//     name: data.primaryName,
-//     knownFor: data.knownForTitles
-//     };
-// });
-//
-// Promise.all([movie_promise, actor_promise]).then((results) => {
-//   let movies = results[0];
-//   let actors = results[1];
+  // Compute default x- and z-domains, and unique the z-domain.
+  if (xDomain === undefined) xDomain = d3.extent(X);
+  if (zDomain === undefined) zDomain = Z;
+  zDomain = new d3.InternSet(zDomain);
 
-// d3.csv("data/actor_dataset.csv")
-//   .row(function(d) {
-//         return {
-//           nid: data.nconst,
-//           name: data.primaryName,
-//           knownFor: data.knownForTitles
-//         };
-//    })
-//   .get(function(data) {
-//       console.log(data);
-//   });
+  // Omit any data not present in the z-domain.
+  const I = d3.range(X.length).filter(i => zDomain.has(Z[i]));
+
+  // Compute a nested array of series where each series is [[y1, y2], [y1, y2],
+  // [y1, y2], …] representing the y-extent of each stacked rect. In addition,
+  // each tuple has an i (index) property so that we can refer back to the
+  // original data point (data[i]). This code assumes that there is only one
+  // data point for a given unique x- and z-value.
+  const series = d3.stack()
+      .keys(zDomain)
+      .value(([x, I], z) => Y[I.get(z)])
+      .order(order)
+      .offset(offset)
+    (d3.rollup(I, ([i]) => i, i => X[i], i => Z[i]))
+    .map(s => s.map(d => Object.assign(d, {i: d.data[1].get(s.key)})));
+
+  // Compute the default y-domain. Note: diverging stacks can be negative.
+  if (yDomain === undefined) yDomain = d3.extent(series.flat(2));
+
+  // Construct scales and axes.
+  const xScale = xType(xDomain, xRange);
+  const yScale = yType(yDomain, yRange);
+  const color = d3.scaleOrdinal(zDomain, colors);
+  const xAxis = d3.axisBottom(xScale).ticks(width / 80, xFormat).tickSizeOuter(0);
+  const yAxis = d3.axisLeft(yScale).ticks(height / 50, yFormat);
+
+  const area = d3.area()
+      .x(({i}) => xScale(X[i]))
+      .y0(([y1]) => yScale(y1))
+      .y1(([, y2]) => yScale(y2));
+
+  const svg = d3.create("svg")
+      .attr("width", width)
+      .attr("height", height)
+      .attr("viewBox", [0, 0, width, height])
+      .attr("style", "max-width: 100%; height: auto; height: intrinsic;");
+
+  svg.append("g")
+    .selectAll("path")
+    .data(series)
+    .join("path")
+      .attr("fill", ([{i}]) => color(Z[i]))
+      .attr("d", area)
+    .append("title")
+      .text(([{i}]) => Z[i]);
+
+  svg.append("g")
+      .attr("transform", `translate(0,${height - marginBottom})`)
+      .call(xAxis)
+      .call(g => g.select(".domain").remove());
+
+  svg.append("g")
+      .attr("transform", `translate(${marginLeft},0)`)
+      .call(yAxis)
+      .call(g => g.select(".domain").remove())
+      .call(g => g.selectAll(".tick line")
+        .filter(d => d === 0 || d === 1)
+        .clone()
+          .attr("x2", width - marginLeft - marginRight))
+      .call(g => g.append("text")
+          .attr("x", -marginLeft)
+          .attr("y", 10)
+          .attr("fill", "currentColor")
+          .attr("text-anchor", "start")
+          .text(yLabel));
+
+  return Object.assign(svg.node(), {scales: {color}});
+}
+
 
 d3.csv("data/movie_dataset.csv").then(function(data) {
   genres = Array.from(new Set(data.map(element => element.genres))).sort();
@@ -158,7 +225,7 @@ d3.csv("data/movie_dataset.csv").then(function(data) {
 
     var tooltip = rankingDiv.append('div').attr('id', 'tooltip')
       .style("position", "absolute")
-      .style("z-index", "10")
+      //.style("z-index", "10")
       .style("visibility", "hidden")
       // .text("Nothing to show");
 
@@ -260,6 +327,10 @@ d3.csv("data/movie_dataset.csv").then(function(data) {
 // =============================================================================
 
   const part2Div = d3.select('#part2div').append('h1').attr('id', 'top').text('Plots');
+
+  // Stacked area chart
+  const stackedDiv = part2Div.append('div').attr('id', 'stacked');
+
   // Part 2: Plots
   const optionsDivPart2 = part2Div.append('div').attr('id', 'options');
   const metricDivPart2 = optionsDivPart2.append('div').attr('id', 'metrics');
@@ -349,6 +420,20 @@ d3.csv("data/movie_dataset.csv").then(function(data) {
   var plotData = prepare_data(data)
   console.log(plotData)
 
+  // var newData = data.filter(function(row) {
+  //   return row['genres'].includes(genre)
+  // });
+  // var newPlotData = prepare_data(newData)
+  // const chart = StackedAreaChart(data, {
+  //   x: d => d.startYear,
+  //   y: d => d.numVotes,
+  //   z: d => d.genres,
+  //   yLabel: "Number of votes",
+  //   width,
+  //   height: 600
+  // });
+  //stackedDiv.append(chart)
+
   // x-axis label
   svg.append("text")
       .attr("transform", "translate(" + ((width/2)-60) + " ," + (height + margin.top) + ")")
@@ -396,6 +481,16 @@ d3.csv("data/movie_dataset.csv").then(function(data) {
   const getPlotData = async (metric, genre) => {
       svg.selectAll("rect").remove()
       svg.selectAll("text").remove()
+
+      // Projector flickering effect
+      for (var i=0; i<50000; i++) {
+        setTimeout(() => {
+            d3.selectAll('.arrow-left').style("visibility", "visible");
+        }, 1750);
+        setTimeout(() => {
+            d3.select('.arrow-left').style("visibility", "hidden");
+        }, 1750);
+      }
 
       if (genre === 'Any') {
         var newPlotData = plotData;
