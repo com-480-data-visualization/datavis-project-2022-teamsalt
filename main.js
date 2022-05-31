b2cf440a04b4e96ecd61d4ee8c40d0a80c5a0d88
Omid Karimi <omid.karimi@epfl.ch>
2022-05-31 18:42:53 +0200

    layout of stacked chart

--- main.js
+++ main.js
@@ -1,116 +1,3 @@
-// Copyright 2021 Observable, Inc.
-// Released under the ISC license.
-// https://observablehq.com/@d3/normalized-stacked-area-chart
-function StackedAreaChart(temp, {
-  x = ([x]) => x, // given d in data, returns the (ordinal) x-value
-  y = ([, y]) => y, // given d in data, returns the (quantitative) y-value
-  z = () => 1, // given d in data, returns the (categorical) z-value
-  marginTop = 20, // top margin, in pixels
-  marginRight = 30, // right margin, in pixels
-  marginBottom = 30, // bottom margin, in pixels
-  marginLeft = 40, // left margin, in pixels
-  width = 640, // outer width, in pixels
-  height = 400, // outer height, in pixels
-  xType = d3.scaleUtc, // type of x-scale
-  xDomain, // [xmin, xmax]
-  xRange = [marginLeft, width - marginRight], // [left, right]
-  yType = d3.scaleLinear, // type of y-scale
-  yDomain, // [ymin, ymax]
-  yRange = [height - marginBottom, marginTop], // [bottom, top]
-  zDomain, // array of z-values
-  offset = d3.stackOffsetExpand, // stack offset method
-  order = d3.stackOrderNone, // stack order method
-  yLabel, // a label for the y-axis
-  xFormat, // a format specifier string for the x-axis
-  yFormat = "%", // a format specifier string for the y-axis
-  colors = d3.schemeTableau10, // an array of colors for the (z) categories
-} = {}) {
-  // Compute values.
-  const X = d3.map(temp, x);
-  const Y = d3.map(temp, y);
-  const Z = d3.map(temp, z);
-  console.log(X)
-  console.log(Y)
-  console.log(Z)
-
-  // Compute default x- and z-domains, and unique the z-domain.
-  if (xDomain === undefined) xDomain = d3.extent(X);
-  if (zDomain === undefined) zDomain = Z;
-  zDomain = new d3.InternSet(zDomain);
-  console.log(xDomain)
-  console.log(zDomain)
-
-  // Omit any data not present in the z-domain.
-  const I = d3.range(X.length).filter(i => zDomain.has(Z[i]));
-
-  // Compute a nested array of series where each series is [[y1, y2], [y1, y2],
-  // [y1, y2], …] representing the y-extent of each stacked rect. In addition,
-  // each tuple has an i (index) property so that we can refer back to the
-  // original data point (data[i]). This code assumes that there is only one
-  // data point for a given unique x- and z-value.
-  const series = d3.stack()
-      .keys(zDomain)
-      .value(([x, I], z) => Y[I.get(z)])
-      .order(order)
-      .offset(offset)
-    (d3.rollup(I, ([i]) => i, i => X[i], i => Z[i]))
-    .map(s => s.map(d => Object.assign(d, {i: d.data[1].get(s.key)})));
-  console.log(series)
-
-  // Compute the default y-domain. Note: diverging stacks can be negative.
-  if (yDomain === undefined) yDomain = d3.extent(series.flat(2));
-  console.log(yDomain)
-
-  // Construct scales and axes.
-  const xScale = xType(xDomain, xRange);
-  const yScale = yType(yDomain, yRange);
-  const color = d3.scaleOrdinal(zDomain, colors);
-  const xAxis = d3.axisBottom(xScale).ticks(width / 80, xFormat).tickSizeOuter(0);
-  const yAxis = d3.axisLeft(yScale).ticks(height / 50, yFormat);
-
-  const area = d3.area()
-      .x(({i}) => xScale(X[i]))
-      .y0(([y1]) => yScale(y1))
-      .y1(([, y2]) => yScale(y2));
-
-  const svg = d3.create("svg")
-      .attr("width", width)
-      .attr("height", height)
-      .attr("viewBox", [0, 0, width, height])
-      .attr("style", "max-width: 100%; height: auto; height: intrinsic;");
-
-  svg.append("g")
-    .selectAll("path")
-    .data(series)
-    //.join("path")
-    .enter().append('path')
-      .attr("fill", ([{i}]) => color(Z[i]))
-      .attr("d", area)
-    .append("title")
-      .text(([{i}]) => Z[i]);
-
-  svg.append("g")
-      .attr("transform", `translate(0,${height - marginBottom})`)
-      .call(xAxis)
-      .call(g => g.select(".domain").remove());
-
-  svg.append("g")
-      .attr("transform", `translate(${marginLeft},0)`)
-      .call(yAxis)
-      .call(g => g.select(".domain").remove())
-      .call(g => g.selectAll(".tick line")
-        .filter(d => d === 0 || d === 1)
-        .clone()
-          .attr("x2", width - marginLeft - marginRight))
-      .call(g => g.append("text")
-          .attr("x", -marginLeft)
-          .attr("y", 10)
-          .attr("fill", "currentColor")
-          .attr("text-anchor", "start")
-          .text(yLabel));
-
-  return Object.assign(svg.node(), {scales: {color}});
-}
 
 d3.csv("data/movie_dataset.csv").then(function(data) {
   genres = Array.from(new Set(data.map(element => element.genres))).sort();
@@ -377,7 +264,7 @@ d3.csv("data/movie_dataset.csv").then(function(data) {
       const currentGenre = document.getElementById('genreSelectPart2').value;
       const currentMetric = document.getElementById('metricSelectPart2').value;
       getPlotData(currentMetric, currentGenre, svg);
-      getPlotData(currentMetric, currentGenre, svg_2);
+      //getPlotData(currentMetric, currentGenre, svg_2);
     });
   metricSelectPart2
     .selectAll('option')
@@ -417,55 +304,98 @@ d3.csv("data/movie_dataset.csv").then(function(data) {
 
   // ====================================================================================================================================================
   // Stacked Area Chart
-  test = []
-  for (var i=0; i < data.length; ++i) {
-      //console.log(data[i].genres)
-      var genreArray = data[i].genres.split(",")
-      //console.log(genreArray)
-      for (var j=0; j < genreArray.length; ++j) {
-          //var date = data[i].startYear.slice(0, -2) + "-01-01";
-          test.push({year: data[i].startYear.slice(0, -2), averageRating: +data[i].averageRating, numVotes: +data[i].numVotes, genre: genreArray[j]});
-      }
-  }
-  console.log(test)
-  test.sort()
 
-  const allKeys = ['Action', 'Adult', 'Adventure', 'Animation', 'Biography', 'Comedy',
-  'Crime', 'Documentary', 'Drama', 'Family', 'Fantasy', 'Game-Show', 'History', 'Horror',
-  'Music', 'Musical', 'Mystery', 'News', 'Reality-TV', 'Romance', 'Sci-Fi', 'Short', 'Sport',
-  'Talk-Show', 'Thriller', 'War', 'Western'];
+  d3.csv("data/counting.csv").then(function(data) {
+    // var svg = d3.select("#my_dataviz")
+    //             .append("svg")
+    var margin = {top: 20, right: 20, bottom: 30, left: 50},
+      width = 960 - margin.left - margin.right,
+      height = 500 - margin.top - margin.bottom;
+    let svg = d3.select("#stacked")
+                .append("svg")
+                .attr("width", width + margin.left + margin.right)
+                .attr("height", height + margin.top + margin.bottom)
+                // .append("g")
+                // .attr("transform",
+                //     "translate(" + margin.left + "," + margin.top + ")");
 
-  const chart = StackedAreaChart(test, {
-    x: d => +d.year,
-    y: d => +d.numVotes,
-    z: d => d.genre,
-    yLabel: "Number of votes",
-    width,
-    height: 600,
-    xType: d3.scaleLinear,
-    xDomain: rangeOfYears(minYear, maxYear),
-    yDomain: rangeOfYears(10000, 260000),
-    zDomain: allKeys
+    function type(d, i, columns) {
+      d.startYear = parseDate(d.startYear);
+      for (var i = 1, n = columns.length; i < n; ++i) d[columns[i]] = d[columns[i]] / 100;
+      return d;
+    }
+
+    console.log(data)
+
+    var keys = data.columns.slice(1);
+
+    var parseDate = d3.timeParse("%Y");
+
+    var stack = d3.stack()
+    //   .keys(keys)
+    //   (data);
+    // console.log(stack)
+
+    var x = d3.scaleTime().range([0, width]),
+        y = d3.scaleLinear().range([height, 0]),
+        z = d3.scaleOrdinal(d3.schemeCategory10);
+
+    var area = d3.area()
+    .x(function(d, i) { return x(d.data.startYear); })
+    .y0(function(d) { return y(d[0]); })
+    .y1(function(d) { return y(d[1]); });
+
+    //var svg = d3.select("svg")
+
+    var g = svg.append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    x.domain(d3.extent(data, function(d) { return d.startYear; }));
+    z.domain(keys);
+    stack.keys(keys);
+    console.log(stack(data))
+
+    var layer = g.selectAll(".layer")
+      .data(stack(data))
+      .enter().append("g")
+        .attr("class", "layer");
+
+    layer.append("path")
+        .attr("class", "area")
+        .style("fill", function(d) { return z(d.key); })
+        .attr("d", area);
+
+    layer.filter(function(d) { return d[d.length - 1][1] - d[d.length - 1][0] > 0.01; })
+      .append("text")
+        .attr("x", width)
+        .attr("y", function(d) { return y((d[d.length - 1][0] + d[d.length - 1][1]) / 2); })
+        .attr("dy", ".35em")
+        .style("font", "10px sans-serif")
+        .style("text-anchor", "end")
+        .text(function(d) { return d.key; });
+
+    g.append("g")
+        .attr("class", "axis axis--x")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x));
+
+    g.append("g")
+        .attr("class", "axis axis--y")
+        .call(d3.axisLeft(y).ticks(10, "%"));
+
   });
-
-  console.log(chart)
-  const svg2 = document.querySelector('svg');
-  var tag = document.createElementNS(chart, "svg");
-  svg2.appendChild(tag)
-  //stackedDiv.append("svg").appendChild(tag);
-  //document.body.appendChild(tag);
   // ====================================================================================================================================================
-
   // Prepare data for plots
   function prepare_data(array) {
       const moviePerYear = d3.groups(array, d => d.startYear).sort()//.filter(d => +d.startYear >= 1990);
       var plotData = [];
+      // iterate through years
       for (var i=0; i < moviePerYear.length; ++i) {
           if (moviePerYear[i][0] >= 1990) {
               let sumVotes = 0;
               let sumRating = 0;
               let count = 0;
-              let genres = []
+              // iterate through movies
               for (var j=0; j < moviePerYear[i][1].length; ++j) {
                   sumVotes += +moviePerYear[i][1][j].numVotes;
                   sumRating += +moviePerYear[i][1][j].averageRating;
@@ -557,36 +487,37 @@ d3.csv("data/movie_dataset.csv").then(function(data) {
               .attr("transform",
                   "translate(" + margin.left + "," + margin.top + ")");
 
-  const genreDivPart2_2 = d3.select("#plot").append('div').attr('id', 'genres');
+  //const genreDivPart2_2 = d3.select("#plot").append('div').attr('id', 'genres');
 
-  let svg_2 = d3.select("#plot")
-              .append("svg")
-              .attr("width", (width + margin.left + margin.right))
-              .attr("height", height + margin.top + margin.bottom)
-              .append("g")
-              .attr("transform",
-                  "translate(" + margin.left + "," + margin.top + ")");
+  // let svg_2 = d3.select("#plot")
+  //             .append("svg")
+  //             .attr("width", (width + margin.left + margin.right))
+  //             .attr("height", height + margin.top + margin.bottom)
+  //             .append("g")
+  //             .attr("transform",
+  //                 "translate(" + margin.left + "," + margin.top + ")");
   // Genre selector for second plot
-  genreDivPart2_2.append('label').attr('class', 'select-label').text('Select Genre:');
-  const genreSelectPart2_2 = genreDivPart2_2
-    .append('select')
-    .attr('id', 'genreSelectPart2_2')
-    .on('change', function (event) {
-      const currentGenre = document.getElementById('genreSelectPart2_2').value;
-      const currentMetric = document.getElementById('metricSelectPart2').value;
-      getPlotData(currentMetric, currentGenre, svg_2);
-    });
-  genreSelectPart2_2
-    .selectAll('option')
-    .data(allGenres)
-    .enter()
-    .append('option')
-    .text(function (d) {
-      return d;
-    })
+  // genreDivPart2_2.append('label').attr('class', 'select-label').text('Select Genre:');
+  // const genreSelectPart2_2 = genreDivPart2_2
+  //   .append('select')
+  //   .attr('id', 'genreSelectPart2_2')
+  //   .on('change', function (event) {
+  //     const currentGenre = document.getElementById('genreSelectPart2_2').value;
+  //     const currentMetric = document.getElementById('metricSelectPart2').value;
+  //     getPlotData(currentMetric, currentGenre, svg_2);
+  //   });
+  // genreSelectPart2_2
+  //   .selectAll('option')
+  //   .data(allGenres)
+  //   .enter()
+  //   .append('option')
+  //   .text(function (d) {
+  //     return d;
+  //   })
 
   create_plot(svg, plotData, "Average Rating")
-  create_plot(svg_2, plotData, "Average Rating")
+  // add eventual other plots
+  //create_plot(svg_2, plotData, "Average Rating")
 
   // UPDATE PLOT
   const getPlotData = async (metric, genre, svg) => {
